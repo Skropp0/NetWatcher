fb36119b43877984c025edbaf34769f685ca0777
tonym <tonymaltas@yahoo.co.uk>
2022-08-19 20:26:25 +0100

    does this work

---
+++ ToastTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+
+Console.WriteLine("Hello, World!");
+
+new ToastContentBuilder()
+    .AddArgument("action", "viewConversation")
+    .AddArgument("conversationId", 9813)
+    .AddText("Hello Will!")
+    .AddText("This is Clem Fandango, can you hear me?")
+    .AddProgressBar("progress", 0.42, valueStringOverride:"valueString", status:"status")
+    .SetToastDuration(ToastDuration.Long)
+    .AddButton("A button", ToastActivationType.Foreground, "args")
+    .Show();
+
+Console.ReadKey();
+//Task.Delay(10000);
+
+Console.WriteLine("...Bye!...");
